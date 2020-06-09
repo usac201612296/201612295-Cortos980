8e49d63c5f40c98feb76c4ae89106ed68d2b6f65
usac201612296 <2811818960101@ingenieria.usac.edu.gt>
2020-06-09 12:38:14 -0600

    ya

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,12 +1,16 @@
-numero = int(296)
+numero = int(19)
+collatz = []
+for i in range(2, 296):
 
-while numero != 1:
-    if numero % 2 == 0:
-        print("%d," % (numero), end = "")
-        numero = numero / 2
-    else:
-        print("%d," % (numero), end = "")
-        numero = (numero * 3) + 1
+    while numero != 1:
+        if numero % 2 == 0:
+            collatz.append(numero)
+            print("%d," % (numero), end = "")
+            numero = numero / 2
+        else:
+            print("%d," % (numero), end = "")
+            numero = (numero * 3) + 1
+            collatz.append(numero)
 
-    if numero == 1:
-        print("1")
+        if numero == 1:
+            print("1")
